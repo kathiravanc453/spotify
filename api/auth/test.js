9f6704fdab43eeb1ac5d5de27ef1d3f9e51a4e80
kathiravanc453 <kathiravanc453@gmail.com>
2026-06-25 09:32:24 +0530

    Update test.js to show all env keys

--- api/auth/test.js
+++ api/auth/test.js
@@ -5,6 +5,6 @@ export default async function handler(req, res) {
   res.status(200).json({
     nodemailer: typeof nodemailer,
     admin: typeof admin,
-    envKeys: Object.keys(process.env).filter(k => k.includes('GMAIL') || k.includes('FIREBASE'))
+    allKeys: Object.keys(process.env).sort()
   });
 }
